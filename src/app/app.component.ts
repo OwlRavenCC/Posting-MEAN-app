d8ade90266344c53e41a23ed3a947b6fd68940c5
Coralia Palacios <cory1599@student.fdu.edu>
2018-12-26 13:28:43 -0800

    Local Post function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MyMEANapp';
+  storedPosts = [];
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
+
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 
 export class PostListComponent {
 
-  public posts = [];
+  @Input() posts = [];
 
 }
 
