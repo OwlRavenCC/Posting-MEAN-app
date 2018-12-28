08bd8858c5562207de2642f881070c2b1ed0fab0
Coralia Palacios <cory1599@student.fdu.edu>
2018-12-28 13:14:58 -0800

    Post Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
 import { MatExpansionModule } from '@angular/material/expansion';
 
